ed94e46b976cdb3f4faeb4f7d6c565021df1573d
hardik8085 <hthakkar@uncc.edu>
2016-10-24 09:23:00 -0400

    User class
    User class in which form we send the data

---
+++ Practise/app/src/main/java/practise/example/com/practise/User.java
@@ -0,0 +1,22 @@
+package practise.example.com.practise;
+
+import java.io.Serializable;
+
+/**
+ * Created by Hardik Thakkar on 1/31/2016.
+ */
+public class User implements Serializable
+{
+    String user;
+    int age;
+
+    User(String user,int age)
+    {
+        super();
+        user=this.user;
+        age=this.age;
+    }
+
+
+
+}
